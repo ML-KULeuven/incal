d2b3c5736e65b576c16fb39b11b96ec7584d079f
Samuel Kolb <samuel.kolb@me.com>
2018-01-29 13:47:43 +0100

    Script fixes

--- scripts/k_generate.sh
+++ scripts/k_generate.sh
@@ -9,4 +9,4 @@ for i in "${options[@]}"; do
 done
 wait
 
-scp -r ../synthetic/kk samuelk@himec04.cs.kuleuven.be:/home/samuelk/projects/smtlearn/output/synthetic/
+scp -r ../synthetic/kk samuelk@himec04.cs.kuleuven.be:/home/samuelk/projects/smtlearn/synthetic/
--- scripts/samples_generate.sh
+++ scripts/samples_generate.sh
@@ -3,4 +3,4 @@
 cd ../smtlearn
 
 python api.py generate ../synthetic/ss/10000 -n 100 -b 6 -r 2 -k 2 -l 3 --h 6 -s 10000
-scp -r ../synthetic/ss samuelk@himec04.cs.kuleuven.be:/home/samuelk/projects/smtlearn/output/synthetic/
+scp -r ../synthetic/ss samuelk@himec04.cs.kuleuven.be:/home/samuelk/projects/smtlearn/synthetic/
