7d528f72af11627d70f431c326d5c6ceb73f86e0
Samuel Kolb <samuel.kolb@me.com>
2018-01-29 13:10:38 +0100

    Script fix

--- scripts/samples_generate.sh
+++ scripts/samples_generate.sh
@@ -2,5 +2,5 @@
 
 cd ../smtlearn
 
-python api.py generate ../synthetic/ss/$i -n 100 -b 6 -r 2 -k 2 -l 3 --h 6 -s 10000
+python api.py generate ../synthetic/ss/10000 -n 100 -b 6 -r 2 -k 2 -l 3 --h 6 -s 10000
 scp -R ../synthetic/ss samuelk@himec04.cs.kuleuven.be:/home/samuelk/projects/smtlearn/output/synthetic/
