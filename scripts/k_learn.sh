8bb12824053db56e41a307f3c5b5f44e3d1d2e3e
Samuel Kolb <samuel.kolb@me.com>
2018-01-29 13:36:39 +0100

    Script fixes

--- scripts/k_learn.sh
+++ scripts/k_learn.sh
@@ -7,6 +7,6 @@ cd ../smtlearn
 declare -a options=(1 2 3 4 5)
 
 for i in "${options[@]}"; do
-    python experiments.py ../synthetic/ss/$i "" ../output/synthetic/kk/$i cnf -t 200 &
+    python experiments.py ../synthetic/kk/$i "" ../output/synthetic/kk/$i cnf -t 200 &
 done
 wait
