0730562479f27344731b0f9883c69979c368e3e7
Samuel Kolb <samuel.kolb@me.com>
2018-05-16 13:09:35 +0200

    Improved logging

--- smtlearn/inc_logging.py
+++ smtlearn/inc_logging.py
@@ -4,6 +4,7 @@ import json
 
 import parse
 from incremental_learner import IncrementalObserver
+from smt_print import pretty_print
 
 
 class LoggingObserver(IncrementalObserver):
@@ -12,15 +13,17 @@ class LoggingObserver(IncrementalObserver):
         self.verbose = verbose
         self.violation_counter = violation_counter
 
-        with open(self.filename, "w") as f:
-            print("", file=f, end="")
+        if filename is not None:
+            with open(self.filename, "w") as f:
+                print("", file=f, end="")
 
         if seed is not None:
             self.log({"type": "seed", "seed": seed})
 
     def log(self, flat):
-        with open(self.filename, "a") as f:
-            print(json.dumps(flat), file=f)
+        if self.filename is not None:
+            with open(self.filename, "a") as f:
+                print(json.dumps(flat), file=f)
 
     def observe_initial(self, initial_indices):
         flat = {"type": "initial", "indices": initial_indices}
@@ -40,9 +43,9 @@ class LoggingObserver(IncrementalObserver):
             flat["violations"] = self.violation_counter.last_violations
         if self.verbose:
             print("Found model after {:.2f}s".format(solving_time))
+            print(pretty_print(theory))
             if self.violation_counter is not None:
                 violation_count = len(self.violation_counter.last_violations)
                 selected_count = len(new_active_indices)
                 print("Selected {} of {} violations in {:.2f}s".format(selected_count, violation_count, selection_time))
-        with open(self.filename, "a") as f:
-            print(json.dumps(flat), file=f)
+        self.log(flat)
