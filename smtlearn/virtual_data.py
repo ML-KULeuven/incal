ef046533f8c3671582e2900a895c0affad2bbd47
Samuel Kolb <samuel.kolb@me.com>
2018-05-16 09:02:14 +0200

    Updated one class strategy

--- smtlearn/virtual_data.py
+++ smtlearn/virtual_data.py
@@ -1,5 +1,9 @@
+from __future__ import print_function
+
+from pysmt.exceptions import InternalSolverError
 from pysmt.environment import Environment
 from pysmt.typing import REAL, BOOL
+from z3types import Z3Exception
 
 from incremental_learner import SelectionStrategy
 
@@ -20,24 +24,47 @@ class OneClassStrategy(SelectionStrategy):
         bool_symbols = {name: fm.Symbol(name, BOOL) for name in domain.bool_vars}
         symbols = real_symbols.copy()
         symbols.update(bool_symbols)
-        with self.environment.factory.Solver() as solver:
-            solver.add_assertion(formula)
-            for row, label in data:
-                if label == self.class_label:
-                    print(len(data), label, self.class_label, self.thresholds, domain.real_vars)
-                    constraint = fm.Implies(fm.And(
-                        *[fm.Iff(bool_symbols[bool_var], fm.Bool(row[bool_var])) for bool_var in domain.bool_vars]),
-                                         fm.Or(*[fm.Ite(real_symbols[real_var] >= fm.Real(row[real_var]),
-                                                        real_symbols[real_var] - fm.Real(row[real_var]) > fm.Real(
-                                                            self.thresholds[real_var]),
-                                                        fm.Real(row[real_var]) - real_symbols[real_var] > fm.Real(
-                                                            self.thresholds[real_var])) for real_var in
-                                                 domain.real_vars]))
-                    print(self.environment.serializer.serialize(constraint))
+        bounds = domain.get_bounds(fm)
+        try:
+            with self.environment.factory.Solver() as solver:
+                solver.add_assertion(formula)
+                solver.add_assertion(bounds)
+                # equalities = []
+                # for row, label in data:
+                #     for real_var in domain.real_vars:
+                #         sym = real_symbols[real_var]
+                #         val = fm.Real(row[real_var])
+                #         t = fm.Real(self.thresholds[real_var])
+                #         equalities.append(fm.Ite(sym >= val, fm.Equals(sym - val, t), fm.Equals(val - sym, t)))
+                # solver.add_assertion(fm.Or(*equalities))
+                for row, label in data:
+                    if label == self.class_label:
+                        constraint = fm.Implies(fm.And(
+                            *[fm.Iff(bool_symbols[bool_var], fm.Bool(row[bool_var])) for bool_var in domain.bool_vars]),
+                            fm.Or(*[fm.Ite(real_symbols[real_var] >= fm.Real(row[real_var]),
+                                           real_symbols[real_var] - fm.Real(row[real_var]) >= fm.Real(
+                                               self.thresholds[real_var]),
+                                           fm.Real(row[real_var]) - real_symbols[real_var] >= fm.Real(
+                                               self.thresholds[real_var])) for real_var in
+                                    domain.real_vars]))
+                    elif label == (not self.class_label):
+                        constraint = fm.Implies(fm.And(
+                            *[fm.Iff(bool_symbols[bool_var], fm.Bool(row[bool_var])) for bool_var in domain.bool_vars]),
+                            fm.Or(*[fm.Ite(real_symbols[real_var] >= fm.Real(row[real_var]),
+                                           real_symbols[real_var] - fm.Real(row[real_var]) >= fm.Real(
+                                               self.thresholds[real_var]),
+                                           fm.Real(row[real_var]) - real_symbols[real_var] >= fm.Real(
+                                               self.thresholds[real_var])) for real_var in
+                                    domain.real_vars]))
                     solver.add_assertion(constraint)
-            solver.solve()
-            model = solver.get_model()
-            example = {var: model.get_value(symbols[var]).constant_value() for var in domain.variables}
+                solver.solve()
+                model = solver.get_model()
+                example = {var: model.get_value(symbols[var]).constant_value() for var in domain.variables}
+                example = {var: float(val) for var, val in example.items() if var in domain.real_vars}
+        except Z3Exception:
+            return None
+        except InternalSolverError:
+            return None
         return example
 
     def select_active(self, domain, data, formula, active_indices):
@@ -46,8 +73,9 @@ class OneClassStrategy(SelectionStrategy):
             return regular_selection
         else:
             example = self.find_violating(domain, data, formula)
+            if example is None:
+                return []
             data.append((example, not self.class_label))
-            print(example)
             return [len(data) - 1]
 
 """
