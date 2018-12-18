8a6bc9c9b322e5c91d21a1667f6ab6d051b8de04
Samuel Kolb <samuel.kolb@me.com>
2018-12-18 17:40:36 +0100

    Fixing evaluation

--- smtlearn/violations/core.py
+++ smtlearn/violations/core.py
@@ -1,21 +1,32 @@
 import random
 
-from pywmi.smt_check import evaluate_assignment
+import numpy as np
+
+from pywmi.smt_check import evaluate
+from pywmi.smt_print import pretty_print, pretty_print_instance
 
 
 class SelectionStrategy(object):
-    def select_active(self, domain, data, formula, active_indices):
+    def select_active(self, domain, data, labels, formula, active_indices):
         raise NotImplementedError()
 
 
 class AllViolationsStrategy(SelectionStrategy):
-    def select_active(self, domain, data, formula, active_indices):
+    def select_active(self, domain, data, labels, formula, active_indices):
         active_set = set(active_indices)
-        for i in range(len(data)):
-            if i not in active_set:
-                learned_label = evaluate_assignment(formula, data[i][0])
-                if learned_label != data[i][1]:
-                    yield i
+        learned_labels = evaluate(domain, formula, data)
+        differences = np.logical_xor(labels, learned_labels)
+        difference_set = set(np.where(differences)[0])
+        # print(active_set)
+        # print(difference_set)
+        # print(pretty_print(formula))
+        # for i in active_set & difference_set:
+        #     print(i)
+        #     print(pretty_print_instance(domain, data[i]))
+        #     print(labels[i], learned_labels[i])
+        # print()
+        # assert len(active_set & difference_set) == 0
+        return sorted(difference_set - active_set)
 
 
 class RandomViolationsStrategy(AllViolationsStrategy):
@@ -23,8 +34,8 @@ class RandomViolationsStrategy(AllViolationsStrategy):
         self.sample_size = sample_size
         self.last_violations = None
 
-    def select_active(self, domain, data, formula, active_indices):
-        all_violations = list(AllViolationsStrategy.select_active(self, domain, data, formula, active_indices))
+    def select_active(self, domain, data, labels, formula, active_indices):
+        all_violations = list(AllViolationsStrategy.select_active(self, domain, data, labels, formula, active_indices))
         self.last_violations = all_violations
         sample_size = min(self.sample_size, len(all_violations))
         return random.sample(all_violations, sample_size)
@@ -36,8 +47,8 @@ class WeightedRandomViolationsStrategy(AllViolationsStrategy):
         self.last_violations = None
         self.weights = weights
 
-    def select_active(self, domain, data, formula, active_indices):
-        all_violations = list(AllViolationsStrategy.select_active(self, domain, data, formula, active_indices))
+    def select_active(self, domain, data, labels, formula, active_indices):
+        all_violations = list(AllViolationsStrategy.select_active(self, domain, data, labels, formula, active_indices))
         self.last_violations = all_violations
         sample_size = min(self.sample_size, len(all_violations))
         import sampling
@@ -50,8 +61,8 @@ class MaxViolationsStrategy(AllViolationsStrategy):
         self.last_violations = None
         self.weights = weights
 
-    def select_active(self, domain, data, formula, active_indices):
-        all_violations = list(AllViolationsStrategy.select_active(self, domain, data, formula, active_indices))
+    def select_active(self, domain, data, labels, formula, active_indices):
+        all_violations = list(AllViolationsStrategy.select_active(self, domain, data, labels, formula, active_indices))
         self.last_violations = all_violations
         sample_size = min(self.sample_size, len(all_violations))
         weighted_violations = zip(all_violations, [self.weights[i] for i in all_violations])
