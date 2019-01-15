702754f3086ba0c4fdae9c4a93d5ed2a0c83a9d4
Samuel Kolb <samuel.kolb@me.com>
2019-01-15 13:22:18 +0100

    - Tweaks to parallel computing

--- incal/util/parallel.py
+++ incal/util/parallel.py
@@ -5,9 +5,9 @@ from multiprocessing.pool import Pool
 def run_command(command):
     process = subprocess.Popen(command, shell=True)
     process.wait()
-    print("completed")
+    print("[complete] {}".format(command))
 
 
 def run_commands(commands):
-    pool = Pool(processes=4)
+    pool = Pool()
     pool.map(run_command, commands)
