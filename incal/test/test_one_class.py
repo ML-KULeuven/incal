040a9d883a007e79603d56d60f8c18a8be611638
Samuel Kolb <samuel.kolb@me.com>
2019-01-25 16:24:42 +0100

    - Added BG knowledge example for INCAL+

--- incal/test/test_one_class.py
+++ incal/test/test_one_class.py
@@ -1,7 +1,7 @@
 from pywmi.smt_print import pretty_print
 
 from incal.learn import LearnOptions
-from pywmi import evaluate
+from pywmi import evaluate, Domain
 from pywmi.sample import uniform
 
 from incal.experiments.examples import simple_checker_problem, checker_problem
@@ -39,5 +39,29 @@ def main():
     print("Data-set grew from {} to {} entries".format(len(labels), len(new_labels)))
 
 
+def test_background_knowledge():
+    domain = Domain.make(["a", "b"], ["x", "y"], [(0, 1), (0, 1)])
+    a, b, x, y = domain.get_symbols(domain.variables)
+    formula = (a | b) & (~a | ~b) & (x >= 0) & (x <= y) & (y <= 1)
+    thresholds = {v: 0.1 for v in domain.real_vars}
+    data = uniform(domain, 10000)
+    labels = evaluate(domain, formula, data)
+    data = data[labels == 1]
+    labels = labels[labels == 1]
+
+    def learn_inc(_data, _labels, _i, _k, _h):
+        strategy = OneClassStrategy(RandomViolationsStrategy(10), thresholds)  #, background_knowledge=(a | b) & (~a | ~b))
+        learner = KCnfSmtLearner(_k, _h, strategy, "mvn")
+        initial_indices = LearnOptions.initial_random(20)(list(range(len(_data))))
+        # learner.add_observer(LoggingObserver(None, _k, _h, None, True))
+        learner.add_observer(PlottingObserver(domain, "test_output/bg", "run_{}_{}_{}".format(_i, _k, _h),
+                                              domain.real_vars[0], domain.real_vars[1], None, False))
+        return learner.learn(domain, _data, _labels, initial_indices)
+
+    (new_data, new_labels, formula), k, h = learn_bottom_up(data, labels, learn_inc, 1, 1, 1, 1, None, None)
+    print("Learned CNF(k={}, h={}) formula {}".format(k, h, pretty_print(formula)))
+    print("Data-set grew from {} to {} entries".format(len(labels), len(new_labels)))
+
+
 if __name__ == "__main__":
-    main()
+    test_background_knowledge()
